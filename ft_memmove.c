c520b99ad67f82289abfb262f2404042e8da6c37
gabrgarc <gabrgarc@student.42sp.org.br>
2025-07-23 18:08:50 -0300

    memmove OK

---
+++ ft_memmove.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 18:10:44 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/07/23 18:03:43 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (src > dest)
+	{
+		i = 0;
+		while (i < n)
+		{
+			((unsigned char *)dest)[i] = ((const unsigned char *)src)[i];
+			i++;
+		}
+		return (dest);
+	}
+	while (n--)
+		((unsigned char *)dest)[n] = ((const unsigned char *)src)[n];
+	return (dest);
+}
