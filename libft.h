dc8a71828b3cba7693b75410fb28bc7ad157da41
gabrgarc <gabrgarc@student.42sp.org.br>
2025-07-23 12:18:17 -0300

    add header

---
+++ libft.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 21:06:07 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/07/21 18:59:32 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+
+size_t	ft_strlen(const char *s);
+
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+
+#endif
