7156da02bf62b0ad8f48004aa268e57c0e5f0653
gabrgarc <gabrgarc@student.42sp.org.br>
2025-07-26 19:18:12 -0300

    memcmp OK

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 18:39:20 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/07/26 19:16:35 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*m1;
+	unsigned char	*m2;
+
+	m1 = (unsigned char *)s1;
+	m2 = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*m1 != *m2)
+			return (*m1 - *m2);
+		m1++;
+		m2++;
+	}
+	return (0);
+}
