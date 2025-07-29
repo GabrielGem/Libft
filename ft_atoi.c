5e45b0bf5c6a6577936c08ac30928b2b813c6af5
gabrgarc <gabrgarc@student.42sp.org.br>
2025-07-29 19:10:39 -0300

    atoi OK

---
+++ ft_atoi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 11:57:12 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/07/29 17:29:47 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	number;
+	int sign;
+
+	while (*nptr == ' ' || (*nptr >= '\t' && *nptr <= '\r'))
+		nptr++;
+	number = 0;
+	sign = 1;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign *= -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+		number = (number * 10) + (*nptr++ - '0');
+	return (number * sign);
+}
