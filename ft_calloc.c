8b133a1822c0b0ede8fb590eabef0c20d6a10b90
gabrgarc <gabrgarc@student.42sp.org.br>
2025-07-29 19:10:57 -0300

    calloc OK

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 14:26:29 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/07/29 17:27:55 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(1));
+	if (nmemb != 0 && size != (nmemb * size / nmemb))
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, size * nmemb);
+	return (ptr);
+}
