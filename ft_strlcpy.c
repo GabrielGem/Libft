02c0807e554b8e2527e8d2cdddbd5a30014532a5
gabrgarc <gabrgarc@student.42sp.org.br>
2025-07-31 18:33:55 -0300

    add guard

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 11:26:48 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/07/31 15:39:04 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/07/31 17:19:18 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	const char	*source;
 
 	if (!src)
-		return (0);	
+		return (0);
 	source = src;
 	if (size > 0)
 	{
