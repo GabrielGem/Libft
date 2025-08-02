6177f5fa0e2af9ea5e62f70beacf367d308564bc
gabrgarc <gabrgarc@student.42sp.org.br>
2025-08-02 17:57:36 -0300

    split OK

--- ft_split.c
+++ ft_split.c
@@ -6,17 +6,14 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 15:00:04 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/08/01 18:43:15 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/08/02 17:51:44 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_word_count(char const *s, char c)
+size_t	ft_word_count(char const *s, char c, size_t wc)
 {
-	size_t	wc;
-
-	wc = 0;
 	while (*s)
 	{
 		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
@@ -26,32 +23,50 @@ size_t	ft_word_count(char const *s, char c)
 	return (wc);
 }
 
-char	**ft_slipt(char const *s, char c)
+void	ft_free_problems(char **str_of_str, size_t n_free)
 {
-	size_t	n_words;
-	char	**strgs;
+	while (n_free--)
+		free(str_of_str[n_free]);
+	free(str_of_str);
+}
+
+static void	ft_aux_split(char **str, char const *s, char c, size_t words)
+{
 	char	*start;
 	char	*end;
+	size_t	ok_alloc;
 
-	if (!s)
-		return (NULL);
-	n_word = ft_word_count(s, c);
-	strgs = malloc((n_word + 1) * sizeof(char *));
 	start = (char *)s;
-	while (n_word--)
+	ok_alloc = 0;
+	while (ok_alloc < words)
 	{
 		while (*start == c)
 			start++;
 		end = start;
 		while (*end != c && *end != '\0')
 			end++;
-		**strgs = malloc(end - start + 1);
-		if (!*strgs)
-			return (NULL);
-		*strgs = ft_substr(s, start, end - start);
-		*strgs++;
+		*str = malloc(end - start + 1);
+		if (!*str)
+			ft_free_problems(str, ok_alloc);
+		ok_alloc++;
+		ft_strlcpy(*str, start, end - start + 1);
+		str++;
 		start = end;
 	}
-	*strgs = '\0';
-	return (&strgs[0]);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	n_words;
+	char	**strgs;
+
+	if (!s)
+		return (NULL);
+	n_words = ft_word_count(s, c, 0);
+	strgs = malloc((n_words + 1) * sizeof(char *));
+	if (!strgs)
+		return (NULL);
+	strgs[n_words] = NULL;
+	ft_aux_split(strgs, s, c, n_words);
+	return (strgs);
 }
