def6203cb33e282a361e1284dc9411fa3dc5eb96
gabrgarc <gabrgarc@student.42sp.org.br>
2025-08-08 15:15:26 -0300

    corrected condition in second loop

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*   By: gabrgarc <gabrgarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 14:53:47 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/07/29 11:46:34 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/08/08 12:54:39 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (big[i] && i < len)
 	{
 		j = 0;
-		while (big[i + j] == little[j] && i + j < len)
+		while (little[j] && big[i + j] == little[j] && i + j < len)
 			j++;
 		if (!little[j])
 			return ((char *)&big[i]);
 		i++;
 	}
-	return ((void *)0);
+	return (NULL);
 }
