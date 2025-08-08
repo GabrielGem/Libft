40ef37ede23b6c7a768aacb728b682ae90f1d941
gabrgarc <gabrgarc@student.42sp.org.br>
2025-08-08 15:20:17 -0300

    altered name of variable i for copyable_size and corrected len in call ft_strlcpy

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabrgarc <gabrgarc@student.42sp.org.b      +#+  +:+       +#+        */
+/*   By: gabrgarc <gabrgarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 18:22:31 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/07/31 17:12:10 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/08/08 12:58:16 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char		*new_str;
 	size_t		len_str;
-	size_t		i;
+	size_t		copyable_size;
 
 	if (!s || start >= ft_strlen(s))
 		return (ft_calloc(1, 1));
 	len_str = ft_strlen(s);
-	i = len_str - start;
-	if (i > len)
-		i = len;
-	new_str = (char *)malloc(i + 1);
+	copyable_size = len_str - start;
+	if (copyable_size > len)
+		copyable_size = len;
+	new_str = (char *)malloc(copyable_size + 1);
 	if (!new_str)
 		return (NULL);
-	ft_strlcpy(new_str, s + start, len + 1);
+	ft_strlcpy(new_str, s + start, copyable_size + 1);
 	return (new_str);
 }
